fcb4f015eff6b8608a67cd6e723a59837dcdbdc4
System administrator <davidh38@github.com>
2021-02-11 20:04:05 +0100

    Update nixos configuration

--- configuration.nix
+++ configuration.nix
@@ -27,7 +27,12 @@ environment.pathsToLink = ["/libexec"];
       wl-clipboard
       mako
       dmenu
+      alacritty
       xwayland
+      docker
+#      telegram-purple
+      slurp #screenshots in wayland
+      wev # used find keyboards symbol names
     ];
   };
 
@@ -56,6 +61,7 @@ etc = {
       isNormalUser = true;
       home = "/home/dave";
       description = "David H";
+      extraGroups = [ "audio" "docker" ];
       #extraGroups = [ "audio" "wheel" "networkmanager" "vboxusers" "docker" ];
     };
   };
@@ -130,8 +136,44 @@ useXkbConfig = true;
 # # List packages installed in system profile. To search, run: # 
 # $ 
 # nix search wget 
-environment.systemPackages = with pkgs; [ wget vim firefox zsh skype git ]; 
+environment.systemPackages = with pkgs; [ wget 
+				       	vim 
+					firefox 
+					zsh 
+					skype 
+					git 
+					discord 
+					emacs 
+					ripgrep 
+					fd 
+					dropbox-cli 
+];
+ networking.firewall = {
+    allowedTCPPorts = [ 17500 ];
+    allowedUDPPorts = [ 17500 ];
+  };
 
+# execute to sync
+# dropbox status
+# dropbox start
+# dropbox autostart y
+  systemd.user.services.dropbox = {
+    description = "Dropbox";
+    wantedBy = [ "graphical-session.target" ];
+    environment = {
+      QT_PLUGIN_PATH = "/run/current-system/sw/" + pkgs.qt5.qtbase.qtPluginPrefix;
+      QML2_IMPORT_PATH = "/run/current-system/sw/" + pkgs.qt5.qtbase.qtQmlPrefix;
+    };
+    serviceConfig = {
+      ExecStart = "${pkgs.dropbox.out}/bin/dropbox";
+      ExecReload = "${pkgs.coreutils.out}/bin/kill -HUP $MAINPID";
+      KillMode = "control-group"; # upstream recommends process
+      Restart = "on-failure";
+      PrivateTmp = true;
+      ProtectSystem = "full";
+      Nice = 10;
+    };
+  };
 # Some programs need SUID wrappers, can be configured further or are # started in user sessions.  # programs.mtr.enable = true; # programs.gnupg.agent = { #   enable = true; 
 # #   enableSSHSupport = true; # }; # List services that you want to enable:
 
